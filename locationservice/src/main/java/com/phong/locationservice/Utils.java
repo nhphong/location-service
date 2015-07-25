014e8a9918474901615077951a94ce2dfe40190d
PhongN+DonN <phong.nguyen@eastagile.com>
2015-07-26 00:23:36 +0700

    - Add Google Play Services SDK - Request permission for FINE_LOCATION and NETWORK_STATE - Utils and LocationServiceApiClient

---
+++ locationservice/src/main/java/com/phong/locationservice/Utils.java
@@ -0,0 +1,138 @@
+package com.phong.locationservice;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+/**
+ * Created by Phong Nguyen on 7/25/15.
+ */
+public class Utils {
+
+    public static final String TAG = Utils.class.getSimpleName();
+
+    public static boolean checkGooglePlayServices(Activity activity) {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
+        final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, activity, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.e(TAG, "This device does not support Google Play Services SDK");
+                activity.finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isGPSEnabled(Context context) {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+        } else {
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+
+    public static void showDialog(Context context, String message, String positiveBtn, DialogInterface.OnClickListener positiveAction) {
+        showDialog(context, message, false, positiveBtn, positiveAction, context.getString(R.string.no), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+    }
+
+    public static void showDialog(Context context, String message, boolean cancelable,
+                                  String positiveBtn, DialogInterface.OnClickListener positiveAction,
+                                  String negativeBtn, DialogInterface.OnClickListener negativeAction) {
+        new AlertDialog.Builder(context)
+                .setMessage(message)
+                .setCancelable(cancelable)
+                .setPositiveButton(positiveBtn, positiveAction)
+                .setNegativeButton(negativeBtn, negativeAction)
+                .create().show();
+    }
+
+    private static ProgressDialog progressDialog;
+    public static synchronized void showProgressDialog(Context context, CharSequence message) {
+        if (progressDialog == null || !progressDialog.isShowing()) {
+            progressDialog = ProgressDialog.show(context, "", message != null ? message : "", true);
+        }
+    }
+
+    public static synchronized void dismissProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
+
+    /**
+     * Checks if the device has Internet connection.
+     *
+     * @return <code>true</code> if the phone is connected to the Internet.
+     */
+    public static boolean checkInternetConnection(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo wifiNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiNetwork != null && wifiNetwork.isConnected()) {
+            return true;
+        }
+
+        NetworkInfo mobileNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (mobileNetwork != null && mobileNetwork.isConnected()) {
+            return true;
+        }
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (activeNetwork != null && activeNetwork.isConnected()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if a Service is still running.
+     */
+    public static boolean isServiceRunning(Context context, Class<?> serviceClass) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : activityManager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.d(TAG, serviceClass + " is running");
+                return true;
+            }
+        }
+        Log.d(TAG, serviceClass + " is NOT running");
+        return false;
+    }
+}
---
+++ locationservice/src/main/java/com/phong/locationservice/api/LocationServiceApiClient.java
@@ -0,0 +1,97 @@
+package com.phong.locationservice.api;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+/**
+ * Created by Phong Nguyen on 7/25/15.
+ */
+public class LocationServiceApiClient implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String TAG = LocationServiceApiClient.class.getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private LocationListener mLocationListener;
+    private boolean mStartLocationUpdatesAfterConnect;
+
+    public LocationServiceApiClient(Context context, long interval, long fastestInterval, int priority, LocationListener listener) {
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = new LocationRequest()
+                .setInterval(interval)
+                .setFastestInterval(fastestInterval)
+                .setPriority(priority);
+
+        mLocationListener = listener;
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected");
+        if (mStartLocationUpdatesAfterConnect) {
+            mStartLocationUpdatesAfterConnect = false;
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended, trying to reconnect");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed with error code: " + connectionResult.getErrorCode());
+    }
+
+    public void connect() {
+        if (!mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    public void connectAndStartLocationUpdates() {
+        if (!mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.connect();
+            mStartLocationUpdatesAfterConnect = true;
+        }
+    }
+
+    public void disconnect() {
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+            Log.d(TAG, "onDisconnected");
+        }
+    }
+
+    public boolean isConnected() {
+        return mGoogleApiClient.isConnected();
+    }
+
+    public void startLocationUpdates() {
+
+        if (!isConnected()) {
+            return;
+        }
+
+        Log.d(TAG, "startLocationUpdates");
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, mLocationListener);
+    }
+
+    public void stopLocationUpdates() {
+        Log.d(TAG, "stopLocationUpdates");
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, mLocationListener);
+    }
+}
