b84ddfda895ebadb6cf5ae09542f42f3cc1bd4dc
PhongN+NgocN <phong.nguyen+ngoc.nguyen@eastagile.com>
2015-08-20 14:12:39 +0700

    temp 1

--- sample/src/main/java/com/phong/sample/MainActivity.java
+++ sample/src/main/java/com/phong/sample/MainActivity.java
@@ -2,8 +2,10 @@ package com.phong.sample;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -12,9 +14,21 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.GeoDataApi;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
@@ -37,10 +51,71 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
 
         Context context = getApplicationContext();
-        try {
-            startActivityForResult(builder.build(context), PLACE_PICKER_REQUEST);
-        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
+//        try {
+//            startActivityForResult(builder.build(context), PLACE_PICKER_REQUEST);
+//        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    private List<PlaceAutocomplete> getAutocomplete(CharSequence constraint) {
+        if (mGoogleApiClient.isConnected()) {
+
+            PendingResult<AutocompletePredictionBuffer> results =
+                    Places.GeoDataApi.getAutocompletePredictions(mGoogleApiClient, constraint.toString(),
+                            new LatLngBounds(new LatLng(8.624472, 102.722168), new LatLng(23.765237, 111.071777)), null);
+
+            // This method should have been called off the main UI thread. Block and wait for at most 60s
+            // for a result from the API.
+            AutocompletePredictionBuffer autocompletePredictions = results
+                    .await(60, TimeUnit.SECONDS);
+
+            // Confirm that the query completed successfully, otherwise return null
+            final Status status = autocompletePredictions.getStatus();
+            if (!status.isSuccess()) {
+                Toast.makeText(this, "Error contacting API: " + status.toString(), Toast.LENGTH_SHORT).show();
+                Log.e("", "Error getting autocomplete prediction API call: " + status.toString());
+                autocompletePredictions.release();
+                return null;
+            }
+
+            Log.i("", "Query completed. Received " + autocompletePredictions.getCount()
+                    + " predictions.");
+
+            // Copy the results into our own data structure, because we can't hold onto the buffer.
+            // AutocompletePrediction objects encapsulate the API response (place ID and description).
+
+            Iterator<AutocompletePrediction> iterator = autocompletePredictions.iterator();
+            ArrayList resultList = new ArrayList<>(autocompletePredictions.getCount());
+            while (iterator.hasNext()) {
+                AutocompletePrediction prediction = iterator.next();
+                // Get the details of this prediction and copy it into a new PlaceAutocomplete object.
+                resultList.add(new PlaceAutocomplete(prediction.getPlaceId(),
+                        prediction.getDescription()));
+            }
+
+            // Release the buffer now that all data has been copied.
+            autocompletePredictions.release();
+
+            return resultList;
+        }
+        Log.e("", "Google API client is not connected for autocomplete query.");
+        return null;
+    }
+
+    class PlaceAutocomplete {
+
+        public CharSequence placeId;
+        public CharSequence description;
+
+        PlaceAutocomplete(CharSequence placeId, CharSequence description) {
+            this.placeId = placeId;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return description.toString();
         }
     }
 
@@ -91,7 +166,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(Bundle bundle) {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                List<PlaceAutocomplete> l = getAutocomplete("ho con rua");
+                return l.get(0).description.toString();
+            }
 
+            @Override
+            protected void onPostExecute(String s) {
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                super.onPostExecute(s);
+            }
+        }.execute();
     }
 
     @Override
